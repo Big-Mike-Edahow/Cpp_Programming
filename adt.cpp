b6cc87519d928dd90852063fbc9f13f7d8b7e1f2
Mike Jackson <perigran_falcon@yahoo.com>
2023-07-31 10:33:34 -0600

    Add files via upload

---
+++ adt.cpp
@@ -0,0 +1,52 @@
+// adt.cpp
+
+#include <iostream>
+
+using namespace std;
+
+class Shape {
+    public:
+        virtual int getArea() = 0;
+        virtual int getEdge() = 0;
+        virtual void Draw() = 0;
+};
+
+class Rect : public Shape {
+    private:
+        int height;
+        int width;
+    public:
+        Rect(int initWidth, int initHeight) {
+            height = initHeight;
+            width = initWidth;
+        }        
+        ~Rect();
+        int getArea() {
+            return height * width;
+        }
+        int getEdge() {
+            return (2 * height) + (2 * width);
+        }
+        void Draw() {
+            for (int i = 0; i < width; i++) {
+                for (int j = 0; j < height; j++) 
+                    {cout << "x "; }
+            cout << endl;}
+        }
+};
+
+int main()
+{
+    Shape* pQuad = new Rect(3,7);
+    Shape* pSquare = new Rect(5,5);
+
+    pQuad -> Draw();
+    cout << "Area is: " << pQuad -> getArea() << endl;
+    cout << "Perimeter is: " <<  pQuad -> getEdge() << endl;
+
+    pSquare -> Draw();
+    cout << "Area is: " << pSquare -> getArea() << endl;
+    cout << "Perimeter is: " << pSquare -> getEdge() << endl;
+
+    return 0;
+}
